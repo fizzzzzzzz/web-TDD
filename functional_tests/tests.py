fca3c3c5610967d40c493f19e1e6173be50045bc
Yu <294109348@qq.com>
2017-09-01 01:21:23 +0800

    new url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -32,6 +32,7 @@ class NewVisitorTest(LiveServerTestCase):
 		inputbox.send_keys('Buy peacock feathers')
 		
 		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 		edith_list_url = self.browser.current_url
 		self.assertRegex(edith_list_url, '/lists/.+')
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
@@ -52,7 +53,7 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertNotIn('make a fly', page_text)
 		
-		inputbox = self.browser.find_element_by_id(id_new_item)
+		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Buy milk')
 		inputbox.send_keys(Keys.ENTER)
 		
